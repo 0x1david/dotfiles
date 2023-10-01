382205e6f70f8c960738da74d2cb34df07b79f79
0x1david <bousi.david@gmail.com>
2023-10-01 22:30:14 +0200

    Scratchpad updates, moving windows between groups keybindings

--- dot_config/qtile/modules/groups.py
+++ dot_config/qtile/modules/groups.py
@@ -1,8 +1,8 @@
 from libqtile.config import DropDown, Group, Key, ScratchPad
-from .keybindings import keys, notes
+from .keybindings import browser_app, keys, notes
 from .preferences import mod, terminal
 from libqtile.lazy import lazy
-
+from libqtile.command import lazy as lazycomm
 # groups = [Group(str(i), screen_affinity=i%3) for i in range(1,7)]
 
 # for it, i in enumerate(groups):
@@ -34,22 +34,26 @@ groups = [
     Group(name="DEV", layout="spiral"),
     Group(name="WWW", layout="spiral"),
     Group(name="DOC", layout="spiral"),
-    ScratchPad("SCR", [
+    ScratchPad("CMD", [
          DropDown("term", terminal, opacity=0.8, x=0.25 ,y=0.15, height=0.7, width=0.5)
     ]),
     ScratchPad("OBS", [
         DropDown("obsidian",'obsidian "obsidian://open?vault=Engineering"'  , opacity=0.8, x=0.25 ,y=0.15, height=0.7, width=0.5)
-    ])
+    ]),
+    ScratchPad("WEB", [
+        DropDown("browser", browser_app + "https://google.com", opacity=0.9, x=0.25 ,y=0.15, height=0.7, width=0.5)
+    ]),
 
 ]
 keys.extend([
             Key([mod], "semicolon", lazy.group["DEV"].toscreen(), desc="Switch to group {}".format("DEV"),),
-            Key([mod, "shift"], "semicolon", lazy.window.togroup("DEV", switch_group=True), desc="Switch to & move focused window to group {}".format("DEV"),),
+            Key([mod, "control"], "semicolon", lazy.window.togroup("DEV", switch_group=True), desc="Switch to & move focused window to group {}".format("DEV"),),
             Key([mod], "apostrophe", lazy.group["WWW"].toscreen(), desc="Switch to group {}".format("WWW"),),
-            Key([mod, "shift"], "apostrophe", lazy.window.togroup("WWW", switch_group=True), desc="Switch to & move focused window to group {}".format("WWW"),),
+            Key([mod, "control"], "apostrophe", lazy.window.togroup("WWW", switch_group=True), desc="Switch to & move focused window to group {}".format("WWW"),),
             Key([mod], "slash", lazy.group["DOC"].toscreen(), desc="Switch to group {}".format("DOC"),),
-            Key([mod, "shift"], "slash", lazy.window.togroup("DOC", switch_group=True), desc="Switch to & move focused window to group {}".format("DOC"),), 
-            Key([], "F12", lazy.group['SCR'].dropdown_toggle('term')),
-            Key([mod], "F12", lazy.group["OBS"].dropdown_toggle('obsidian'))
+            Key([mod, "control"], "slash", lazy.window.togroup("DOC", switch_group=True), desc="Switch to & move focused window to group {}".format("DOC"),), 
+            Key([mod], "F11", lazy.group['CMD'].dropdown_toggle('term')),
+            Key([], "F12", lazy.group['WEB'].dropdown_toggle('browser')),
+            Key([mod], "F12", lazy.group["OBS"].dropdown_toggle('obsidian')),
                 ]
             )
--- dot_config/qtile/modules/keybindings.py
+++ dot_config/qtile/modules/keybindings.py
@@ -1,6 +1,7 @@
 from libqtile.command import lazy
 from libqtile.config import Key, Click, Drag
 from .preferences import terminal, browser, browser_app, mod, notes
+from .scripts import switch_group 
 
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
@@ -17,18 +18,20 @@ keys = [
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
+
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
 
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    # Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    # Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    # Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    # Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
@@ -56,4 +59,5 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+
 ]
